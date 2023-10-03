cca5f97654919f31bf42edf765ff95ee1c02d8af
Ayax Garcia Williner <agarciawilliner@gmail.com>
2023-10-03 00:52:05 -0300

    ...

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+function About() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+            Electrobank es tu banco en línea de confianza, ofreciendo servicios
+            bancarios seguros y convenientes.
+        </p>
+      </div>
+    );
+  }
+  
+  export default About
---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <p>
+            Acá va a ir el login de la página...
+        </p>
+      </div>
+    );
+  }
+  
+  export default Login
